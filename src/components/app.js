74f3764e76e8c7f4d3f3dce41ff819071768151e
rochakjain361 <rochak_r@ec.iitr.ac.in>
2022-01-14 22:56:25 +0530

    Update app header

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Scrollbars } from 'react-custom-scrollbars'
-import { Segment, Container } from 'semantic-ui-react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import { appBaseUrl, urlProfile } from '../urls'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Scrollbars } from "react-custom-scrollbars";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import { AppHeader, AppFooter, AppMain, getTheme } from 'formula_one'
-import Profile from './Profile/profile'
-import Search from './Search/search'
-import main from 'formula_one/src/css/app.css'
-import blocks from '../css/app.css'
+import { AppHeader, AppFooter, AppMain } from "formula_one";
+import Profile from "./Profile/profile";
+import Search from "./Search/search";
+import main from "formula_one/src/css/app.css";
+import blocks from "../css/app.css";
 
 class App extends Component {
   render() {
@@ -17,50 +15,41 @@ class App extends Component {
       {
         name: "Anshul Dutt Sharma",
         role: "Full Stack Developer",
-        link: 'https://internet.channeli.in/maintainer_site/team/Anshul/',
+        link: "https://internet.channeli.in/maintainer_site/team/Anshul/",
       },
       {
-        name: 'Pushpam Choudhary',
-        role: 'Frontend developer',
+        name: "Pushpam Choudhary",
+        role: "Frontend developer",
       },
       {
-        name: 'Gagan Sharma',
+        name: "Gagan Sharma",
         role: "Full Stack Developer",
         link: "https://channeli.in/maintainer_site/team/gagan_sharma/",
-      }
-    ]
+      },
+    ];
 
     return (
-      <div styleName='main.app'>
-        <AppHeader appName='people_search' mode='app' />
-        <AppMain styleName='blocks.main'>
-          <div styleName='main.app-main'>
+      <div styleName="main.app">
+        <AppHeader appName="people_search" mode="app" userDropdown />
+        <AppMain styleName="blocks.main">
+          <div styleName="main.app-main">
             <Scrollbars autoHide>
               <Switch>
-                <Route
-                  exact
-                  path="/people_search"
-                  component={Search}
-                />
+                <Route exact path="/people_search" component={Search} />
                 <Route
                   exact
                   path="/people_search/profile/"
                   component={Profile}
                 />
-                <Route render={props => <Redirect to='/404' />} />
+                <Route render={(props) => <Redirect to="/404" />} />
               </Switch>
-
-
             </Scrollbars>
           </div>
         </AppMain>
         <AppFooter creators={creators} />
       </div>
-    )
+    );
   }
 }
 
-export default connect(
-  null,
-  null
-)(App)
+export default connect(null, null)(App);
