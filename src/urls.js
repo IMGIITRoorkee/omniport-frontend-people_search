dcd1f35b560a7f354b4a6bd1179b9908267d869b
rochakjain361 <rochak_r@ec.iitr.ac.in>
2022-01-24 15:53:05 +0530

    Update index, urls and add store for redux reducers

--- src/urls.js
+++ src/urls.js
@@ -1,32 +1,29 @@
-import config from '../config.json'
+import config from "../config.json";
 
 export const appBaseUrl = () => {
-  return config.baseUrl
-}
+  return config.baseUrl;
+};
 
 export const urlProfile = () => {
-  return `${appBaseUrl()}/profile/`
-}
+  return `${appBaseUrl()}/profile/`;
+};
 
 export const urlOptions = () => {
-  return `/api/people_search/advanced_search/`
-}
+  return `/api/people_search/advanced_search/`;
+};
 
 export const getWhoAmIApi = () => {
-  return '/kernel/who_am_i/'
-}
+  return "/kernel/who_am_i/";
+};
 export const urlStudentQuery = () => {
-  return `/api/people_search/student_search/`
-}
+  return `/api/people_search/student_search/`;
+};
 export const urlFacultyQuery = () => {
-  return `/api/people_search/faculty_search/`
-}
-export const urlInterestQuery = () => {
-  return `/api/people_search/interest_search/`
-}
+  return `/api/people_search/faculty_search/`;
+};
 export const urlFacultyProfile = (id) => {
-  return `${urlFacultyQuery()}${id}`
-}
+  return `${urlFacultyQuery()}${id}`;
+};
 export const urlStudentProfile = (id) => {
-  return `/api/people_search/student_detail_search/${id}/`
-}
+  return `/api/people_search/student_search/${id}/`;
+};
